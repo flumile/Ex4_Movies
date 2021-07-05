82dab847ff9de69e220c2dc29a5783fefe6c95c2
flumile <millionmiles17@hotmail.com>
2021-07-05 17:28:12 +0700

    done

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import allMovies from './movies.json'
 import './main.scss'
 
 document.addEventListener('DOMContentLoaded',function(){
-    const set = 10
+    const set = 50
     const movie = document.getElementById('cards')
     const movies = allMovies.slice(0,set)
     console.log(movies)
 
     movies.forEach(num => {
-        console.log(num.tomatoScore)
-        const rateImg = "sfdfj"
+        const rateImg = getRateIMG(num.tomatoIcon)
         const movieDom = `<div class="card">
         <img class="main-img" src="${num.posters.primary}" alt="">
         <div class="title">${num.title}</div>
@@ -22,3 +21,11 @@ document.addEventListener('DOMContentLoaded',function(){
         movie.insertAdjacentHTML('beforeend',movieDom)
     })
 })
+
+function getRateIMG(iconName){
+    let ic=iconName
+    console.log(ic)
+    if(ic === "fresh"){return 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/tomatometer/certified_fresh-notext.56a89734a59.svg'}
+    else if(ic === "certified_fresh"){return 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/tomatometer/tomatometer-fresh.149b5e8adc3.svg'}
+    else if(ic === "rotten"){return 'https://www.rottentomatoes.com/assets/pizza-pie/images/icons/tomatometer/tomatometer-rotten.f1ef4f02ce3.svg'}
+}
